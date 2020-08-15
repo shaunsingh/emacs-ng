3b58bfce7b6b17766f270c990cb071a409945110
Harry Fei <tiziyuanfang@gmail.com>
2020-08-15 21:09:24 +0800

    webrender: add a empty `xw-color-values` lisp function

--- rust_src/src/wrterm.rs
+++ rust_src/src/wrterm.rs
@@ -441,6 +441,12 @@ pub fn x_display_grayscale_p(_terminal: LispObject) -> bool {
     unimplemented!();
 }
 
+/// Internal function called by `color-values', which see.
+#[lisp_fn(min = "1")]
+pub fn xw_color_values(_color: LispObject, _frame: Option<LispFrameRef>) -> LispObject {
+    Qnil
+}
+
 fn syms_of_wrfont() {
     unsafe {
         register_font_driver(FONT_DRIVER.clone().as_mut(), ptr::null_mut());
