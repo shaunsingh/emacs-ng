fangyuanziti/remacs

webrender: enable x term feature

rust_src/src/wrterm.rs
@@ -9,8 +9,9 @@ use crate::{
     frame::LispFrameRef,
     lisp::{ExternalPtr, LispObject},
     remacs_sys::{
-        font, wr_display_info, wr_output, Display, Pixmap, Qnil, WRImage, Window, XColor,
-        XrmDatabase,
+        font, hashtest_eql, make_hash_table, wr_display_info, wr_output, Display, Fprovide, Pixmap,
+        Qnil, Qt, Qwr, Qx, WRImage, Window, XColor, XrmDatabase, DEFAULT_REHASH_SIZE,
+        DEFAULT_REHASH_THRESHOLD,
     },
 };
 
@@ -167,9 +168,52 @@ pub fn x_hide_tip() -> bool {
     unimplemented!();
 }
 
+/// Make a new X window, which is called a "frame" in Emacs terms.
+/// Return an Emacs frame object.  PARMS is an alist of frame parameters.
+/// If the parameters specify that the frame should not have a minibuffer,
+/// and do not specify a specific minibuffer window to use, then
+/// `default-minibuffer-frame' must be a frame whose minibuffer can be
+/// shared by the new frame.
+///
+/// This function is an internal primitive--use `make-frame' instead.
+#[lisp_fn]
+pub fn x_create_frame(_parms: LispObject) {
+    unimplemented!();
+}
+
 #[no_mangle]
+#[allow(unused_doc_comments)]
 pub extern "C" fn syms_of_wrterm() {
+    // pretend webrender as a X gui backend, so we can reuse the x-win.el logic
+    def_lisp_sym!(Qx, "x");
     def_lisp_sym!(Qwr, "wr");
+    unsafe {
+        Fprovide(Qx, Qnil);
+        Fprovide(Qwr, Qnil);
+    }
+
+    let x_keysym_table = unsafe {
+        make_hash_table(
+            hashtest_eql.clone(),
+            900,
+            DEFAULT_REHASH_SIZE,
+            DEFAULT_REHASH_THRESHOLD,
+            Qnil,
+            false,
+        )
+    };
+
+    /// Hash table of character codes indexed by X keysym codes.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_keysym_table, "x-keysym-table", x_keysym_table);
+
+    /// Which toolkit scroll bars Emacs uses, if any.
+    /// A value of nil means Emacs doesn't use toolkit scroll bars.
+    /// With the X Window system, the value is a symbol describing the
+    /// X toolkit.  Possible values are: gtk, motif, xaw, or xaw3d.
+    /// With MS Windows or Nextstep, the value is t.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_toolkit_scroll_bars, "x-toolkit-scroll-bars", Qt);
 }
 
 include!(concat!(env!("OUT_DIR"), "/wrterm_exports.rs"));

shaunsingh/emacs-ng

rust_src/src/wrterm.rs
@@ -8,8 +8,9 @@ use crate::{
     frame::LispFrameRef,
     lisp::{ExternalPtr, LispObject},
     remacs_sys::{
-        font, image, wr_display_info, wr_output, Display, Emacs_Pixmap, Pixmap, Qnil, WRImage,
-        Window, XrmDatabase,
+        font, hashtest_eql, image, make_hash_table, wr_display_info, wr_output, Display,
+        Emacs_Pixmap, Fprovide, Pixmap, Qnil, Qwr, Qx, WRImage, Window, XrmDatabase,
+        DEFAULT_REHASH_SIZE, DEFAULT_REHASH_THRESHOLD,
     },
 };
 
@@ -174,9 +175,52 @@ pub fn x_hide_tip() -> bool {
     unimplemented!();
 }
 
+/// Make a new X window, which is called a "frame" in Emacs terms.
+/// Return an Emacs frame object.  PARMS is an alist of frame parameters.
+/// If the parameters specify that the frame should not have a minibuffer,
+/// and do not specify a specific minibuffer window to use, then
+/// `default-minibuffer-frame' must be a frame whose minibuffer can be
+/// shared by the new frame.
+///
+/// This function is an internal primitive--use `make-frame' instead.
+#[lisp_fn]
+pub fn x_create_frame(_parms: LispObject) {
+    unimplemented!();
+}
+
 #[no_mangle]
+#[allow(unused_doc_comments)]
 pub extern "C" fn syms_of_wrterm() {
+    // pretend webrender as a X gui backend, so we can reuse the x-win.el logic
+    def_lisp_sym!(Qx, "x");
     def_lisp_sym!(Qwr, "wr");
+    unsafe {
+        Fprovide(Qx, Qnil);
+        Fprovide(Qwr, Qnil);
+    }
+
+    let x_keysym_table = unsafe {
+        make_hash_table(
+            hashtest_eql.clone(),
+            900,
+            DEFAULT_REHASH_SIZE,
+            DEFAULT_REHASH_THRESHOLD,
+            Qnil,
+            false,
+        )
+    };
+
+    // Hash table of character codes indexed by X keysym codes.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_keysym_table, "x-keysym-table", x_keysym_table);
+
+    // Which toolkit scroll bars Emacs uses, if any.
+    // A value of nil means Emacs doesn't use toolkit scroll bars.
+    // With the X Window system, the value is a symbol describing the
+    // X toolkit.  Possible values are: gtk, motif, xaw, or xaw3d.
+    // With MS Windows or Nextstep, the value is t.
+    #[rustfmt::skip]
+    defvar_lisp!(Vx_toolkit_scroll_bars, "x-toolkit-scroll-bars", Qnil);
 }
 
 include!(concat!(env!("OUT_DIR"), "/wrterm_exports.rs"));