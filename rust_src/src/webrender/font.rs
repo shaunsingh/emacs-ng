fangyuanziti/remacs

webrender: implement basic `x-create-frame`

rust_src/src/webrender/font.rs
@@ -1,0 +1,107 @@
+use std::ptr;
+
+use crate::{
+    frame::LispFrameRef,
+    lisp::{ExternalPtr, LispObject},
+    remacs_sys::{font, font_driver, font_metrics, frame, glyph_string, Qwr},
+};
+
+use super::output::OutputRef;
+
+pub type FontRef = ExternalPtr<font>;
+impl Default for FontRef {
+    fn default() -> Self {
+        FontRef::new(ptr::null_mut())
+    }
+}
+
+type FontDriverRef = ExternalPtr<font_driver>;
+unsafe impl Sync for FontDriverRef {}
+
+lazy_static! {
+    pub static ref FONT_DRIVER: FontDriverRef = {
+        let mut font_driver = Box::new(font_driver::default());
+
+        font_driver.type_ = Qwr;
+        font_driver.case_sensitive = true;
+        font_driver.get_cache = Some(get_cache);
+        font_driver.list = Some(list);
+        font_driver.match_ = Some(match_);
+        font_driver.list_family = Some(list_family);
+        font_driver.open = Some(open);
+        font_driver.close = Some(close);
+        font_driver.has_char = Some(has_char);
+        font_driver.encode_char = Some(encode_char);
+        font_driver.text_extents = Some(text_extents);
+        font_driver.draw = Some(draw);
+
+        FontDriverRef::new(Box::into_raw(font_driver))
+    };
+}
+
+#[allow(unused_variables)]
+extern "C" fn get_cache(f: *mut frame) -> LispObject {
+    let frame = LispFrameRef::new(f);
+    let output: OutputRef = unsafe { frame.output_data.wr.into() };
+
+    let dpyinfo = output.get_inner().display_info;
+
+    dpyinfo.name_list_element
+}
+
+#[allow(unused_variables)]
+extern "C" fn draw(
+    s: *mut glyph_string,
+    from: i32,
+    to: i32,
+    x: i32,
+    y: i32,
+    with_backgroud: bool,
+) -> i32 {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn list(frame: *mut frame, font_spec: LispObject) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn match_(f: *mut frame, spec: LispObject) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn list_family(f: *mut frame) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn open(f: *mut frame, font_entity: LispObject, pixel_size: i32) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn close(font: *mut font) {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn has_char(font: LispObject, c: i32) -> i32 {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn encode_char(font: *mut font, c: i32) -> u32 {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn text_extents(
+    font: *mut font,
+    code: *mut u32,
+    nglyphs: i32,
+    metrics: *mut font_metrics,
+) {
+    unimplemented!();
+}

rust_src/src/webrender/term.rs
@@ -5,8 +5,9 @@ use super::display_info::{DisplayInfo, DisplayInfoRef};
 use crate::{
     lisp::{ExternalPtr, LispObject},
     remacs_sys::{
-        allocate_kboard, create_terminal, current_kboard, initial_kboard, output_method, terminal,
-        xlispstrdup, Fcons, Qnil, Qwr, KBOARD,
+        allocate_kboard, create_terminal, current_kboard, frame_parm_handler, initial_kboard,
+        output_method, redisplay_interface, terminal, x_set_font, x_set_font_backend, xlispstrdup,
+        Fcons, Qnil, Qwr, KBOARD,
     },
 };
 
@@ -26,8 +27,107 @@ impl KboardRef {
     }
 }
 
+type RedisplayInterfaceRef = ExternalPtr<redisplay_interface>;
+unsafe impl Sync for RedisplayInterfaceRef {}
+
+fn get_frame_parm_handlers() -> [frame_parm_handler; 45] {
+    // Keep this list in the same order as frame_parms in frame.c.
+    // Use None for unsupported frame parameters.
+    let handlers: [frame_parm_handler; 45] = [
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(x_set_font),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(x_set_font_backend),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    ];
+
+    handlers
+}
+
+lazy_static! {
+    static ref REDISPLAY_INTERFACE: RedisplayInterfaceRef = {
+        let frame_parm_handlers = Box::new(get_frame_parm_handlers());
+
+        let interface = Box::new(redisplay_interface {
+            frame_parm_handlers: (Box::into_raw(frame_parm_handlers)) as *mut Option<_>,
+            produce_glyphs: None,
+            write_glyphs: None,
+            insert_glyphs: None,
+            clear_end_of_line: None,
+            scroll_run_hook: None,
+            after_update_window_line_hook: None,
+            update_window_begin_hook: None,
+            update_window_end_hook: None,
+            flush_display: None,
+            clear_window_mouse_face: None,
+            get_glyph_overhangs: None,
+            fix_overlapping_area: None,
+            draw_fringe_bitmap: None,
+            define_fringe_bitmap: None,
+            destroy_fringe_bitmap: None,
+            compute_glyph_string_overhangs: None,
+            draw_glyph_string: None,
+            define_frame_cursor: None,
+            clear_frame_area: None,
+            draw_window_cursor: None,
+            draw_vertical_window_border: None,
+            draw_window_divider: None,
+            shift_glyphs_for_insert: None,
+            show_hourglass: None,
+            hide_hourglass: None,
+        });
+
+        RedisplayInterfaceRef::new(Box::into_raw(interface))
+    };
+}
+
 fn wr_create_terminal(mut dpyinfo: DisplayInfoRef) -> TerminalRef {
-    let terminal_ptr = unsafe { create_terminal(output_method::output_wr, ptr::null_mut()) };
+    let terminal_ptr = unsafe {
+        create_terminal(
+            output_method::output_wr,
+            REDISPLAY_INTERFACE.clone().as_mut(),
+        )
+    };
     let mut terminal = TerminalRef::new(terminal_ptr);
 
     // Link terminal and dpyinfo together

shaunsingh/emacs-ng

rust_src/src/webrender/font.rs
@@ -1,0 +1,109 @@
+use std::ptr;
+
+use lazy_static::lazy_static;
+
+use crate::{
+    frame::LispFrameRef,
+    lisp::{ExternalPtr, LispObject},
+    remacs_sys::{font, font_driver, font_metrics, frame, glyph_string, Qwr},
+};
+
+use super::output::OutputRef;
+
+pub type FontRef = ExternalPtr<font>;
+impl Default for FontRef {
+    fn default() -> Self {
+        FontRef::new(ptr::null_mut())
+    }
+}
+
+type FontDriverRef = ExternalPtr<font_driver>;
+unsafe impl Sync for FontDriverRef {}
+
+lazy_static! {
+    pub static ref FONT_DRIVER: FontDriverRef = {
+        let mut font_driver = Box::new(font_driver::default());
+
+        font_driver.type_ = Qwr;
+        font_driver.case_sensitive = true;
+        font_driver.get_cache = Some(get_cache);
+        font_driver.list = Some(list);
+        font_driver.match_ = Some(match_);
+        font_driver.list_family = Some(list_family);
+        font_driver.open_font = Some(open_font);
+        font_driver.close_font = Some(close_font);
+        font_driver.has_char = Some(has_char);
+        font_driver.encode_char = Some(encode_char);
+        font_driver.text_extents = Some(text_extents);
+        font_driver.draw = Some(draw);
+
+        FontDriverRef::new(Box::into_raw(font_driver))
+    };
+}
+
+#[allow(unused_variables)]
+extern "C" fn get_cache(f: *mut frame) -> LispObject {
+    let frame = LispFrameRef::new(f);
+    let output: OutputRef = unsafe { frame.output_data.wr.into() };
+
+    let dpyinfo = output.get_inner().display_info;
+
+    dpyinfo.name_list_element
+}
+
+#[allow(unused_variables)]
+extern "C" fn draw(
+    s: *mut glyph_string,
+    from: i32,
+    to: i32,
+    x: i32,
+    y: i32,
+    with_backgroud: bool,
+) -> i32 {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn list(frame: *mut frame, font_spec: LispObject) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn match_(f: *mut frame, spec: LispObject) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn list_family(f: *mut frame) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn open_font(f: *mut frame, font_entity: LispObject, pixel_size: i32) -> LispObject {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn close_font(font: *mut font) {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn has_char(font: LispObject, c: i32) -> i32 {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn encode_char(font: *mut font, c: i32) -> u32 {
+    unimplemented!();
+}
+
+#[allow(unused_variables)]
+extern "C" fn text_extents(
+    font: *mut font,
+    code: *const u32,
+    nglyphs: i32,
+    metrics: *mut font_metrics,
+) {
+    unimplemented!();
+}

rust_src/src/webrender/term.rs
@@ -1,11 +1,14 @@
 use std::ptr;
 
+use lazy_static::lazy_static;
+
 use super::display_info::{DisplayInfo, DisplayInfoRef};
 
 use crate::{
     lisp::{ExternalPtr, LispObject},
     remacs_sys::{
-        allocate_kboard, create_terminal, current_kboard, initial_kboard, output_method, terminal,
+        allocate_kboard, create_terminal, current_kboard, frame_parm_handler, gui_set_font,
+        gui_set_font_backend, initial_kboard, output_method, redisplay_interface, terminal,
         xlispstrdup, Fcons, Qnil, Qwr, KBOARD,
     },
 };
@@ -26,6 +29,102 @@ impl KboardRef {
     }
 }
 
+type RedisplayInterfaceRef = ExternalPtr<redisplay_interface>;
+unsafe impl Sync for RedisplayInterfaceRef {}
+
+fn get_frame_parm_handlers() -> [frame_parm_handler; 45] {
+    // Keep this list in the same order as frame_parms in frame.c.
+    // Use None for unsupported frame parameters.
+    let handlers: [frame_parm_handler; 45] = [
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(gui_set_font),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(gui_set_font_backend),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    ];
+
+    handlers
+}
+
+lazy_static! {
+    static ref REDISPLAY_INTERFACE: RedisplayInterfaceRef = {
+        let frame_parm_handlers = Box::new(get_frame_parm_handlers());
+
+        let interface = Box::new(redisplay_interface {
+            frame_parm_handlers: (Box::into_raw(frame_parm_handlers)) as *mut Option<_>,
+            produce_glyphs: None,
+            write_glyphs: None,
+            insert_glyphs: None,
+            clear_end_of_line: None,
+            clear_under_internal_border: None,
+            scroll_run_hook: None,
+            after_update_window_line_hook: None,
+            update_window_begin_hook: None,
+            update_window_end_hook: None,
+            flush_display: None,
+            clear_window_mouse_face: None,
+            get_glyph_overhangs: None,
+            fix_overlapping_area: None,
+            draw_fringe_bitmap: None,
+            define_fringe_bitmap: None,
+            destroy_fringe_bitmap: None,
+            compute_glyph_string_overhangs: None,
+            draw_glyph_string: None,
+            define_frame_cursor: None,
+            default_font_parameter: None,
+            clear_frame_area: None,
+            draw_window_cursor: None,
+            draw_vertical_window_border: None,
+            draw_window_divider: None,
+            shift_glyphs_for_insert: None,
+            show_hourglass: None,
+            hide_hourglass: None,
+        });
+
+        RedisplayInterfaceRef::new(Box::into_raw(interface))
+    };
+}
+
 extern "C" fn get_string_resource(
     _rdb: *mut libc::c_void,
     _name: *const libc::c_char,
@@ -35,7 +134,12 @@ extern "C" fn get_string_resource(
 }
 
 fn wr_create_terminal(mut dpyinfo: DisplayInfoRef) -> TerminalRef {
-    let terminal_ptr = unsafe { create_terminal(output_method::output_wr, ptr::null_mut()) };
+    let terminal_ptr = unsafe {
+        create_terminal(
+            output_method::output_wr,
+            REDISPLAY_INTERFACE.clone().as_mut(),
+        )
+    };
     let mut terminal = TerminalRef::new(terminal_ptr);
 
     // Link terminal and dpyinfo together